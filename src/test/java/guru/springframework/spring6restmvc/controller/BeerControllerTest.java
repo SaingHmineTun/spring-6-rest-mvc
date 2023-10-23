3ab8f1c40b2e2441e4c20610412757686793bd75
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2023-10-23 21:41:17 +0630

    Updated BeerControllerTest Using jsonPath to assert the returned json text.

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -11,11 +11,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
@@ -29,12 +29,14 @@ class BeerControllerTest {
     @Test
     void test_getBeerById() throws Exception {
 
-        given(beerService.getBeerById(any(UUID.class))).willReturn(Beer.builder().id(UUID.randomUUID()).build());
+
+        given(beerService.getBeerById(any(UUID.class))).willReturn(Beer.builder().id(UUID.randomUUID()).beerName("Change").build());
 
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$['beerName']", is("Change")));
     }
 
 }
