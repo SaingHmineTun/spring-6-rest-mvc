c9b9f6988033e8e5c898fda344aae3c6ce7160ad
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2023-10-23 21:09:53 +0630

    Added - BeerControllerTest.java

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -0,0 +1,39 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.service.BeerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @MockBean
+    BeerService beerService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void test_getBeerById() throws Exception {
+
+        given(beerService.getBeerById(any(UUID.class))).willReturn(Beer.builder().build());
+
+        mockMvc.perform(get("/" + UUID.randomUUID())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
