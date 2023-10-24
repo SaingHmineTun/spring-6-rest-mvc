3459fb6d257f314d2fc13bd136ba32f6213eb08e
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2023-10-24 23:01:53 +0630

    Implement Services for Jpa! Add BootstrapData.java to pre-filled required data!

---
+++ src/main/java/guru/springframework/spring6restmvc/service/BeerServiceJpa.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring6restmvc.service;
+
+import guru.springframework.spring6restmvc.mappers.BeerMapper;
+import guru.springframework.spring6restmvc.model.BeerDTO;
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Primary
+@Service
+@RequiredArgsConstructor
+public class BeerServiceJpa implements BeerService {
+
+    public final BeerRepository beerRepository;
+    public final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return null;
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public BeerDTO addBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public boolean updateBeer(UUID id, BeerDTO beer) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteBeerById(UUID uuid) {
+        return false;
+    }
+
+    @Override
+    public boolean updateBeerContentById(UUID uuid, BeerDTO beer) {
+        return false;
+    }
+}
