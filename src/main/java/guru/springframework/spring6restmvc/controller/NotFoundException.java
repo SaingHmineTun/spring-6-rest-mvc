9892a2110cc473728031faafdc38d95cfa852c2b
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2023-10-28 21:26:27 +0630

    Changed from using XAMP to MySQL. Changed Database username & password. Adding Flyway dependencies.

--- src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
@@ -8,7 +8,6 @@ public class NotFoundException extends RuntimeException{
     public NotFoundException() {
         super();
     }
-
     public NotFoundException(String message) {
         super(message);
     }
